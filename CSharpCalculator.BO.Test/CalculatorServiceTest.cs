186b1274ae4625751fde12d23437c890e5cf432c
Maryna Kabachenko <Maryna_Kabachenko@epam.com>
2017-05-18 20:37:19 +0200

    Add project files.

---
+++ CSharpCalculator.BO.Test/CalculatorServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSharpCalculator.BO;
+
+namespace CSharpCalculator.BO.Test
+{
+    [TestFixture] //contains unit test, all in [] are attribute. Attribute is a marker
+    public class CalculatorServiceTest
+    {
+        [TestCase("1", "2", "13")] // says that below is test
+        [TestCase("1", "3", "13")]
+        [TestCase("1", "24", "124")]
+        [TestCase("0", "2", "02")]
+        public void ParseInputTest(string prevoiusInput, string currentInput, string expectedResult)
+        {
+            //arrange
+            //var expectedResult = "13";
+
+            //Act
+            var actualResult = CalculatorService.ParseInput(prevoiusInput, currentInput);
+                                
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+           // Assert.That(actualResult = expectedResult, Throws.Exception );
+        }
+
+    }
+}
---
+++ CSharpCalculator.BO/CalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCalculator.BO
+{
+    public static class CalculatorService
+    {
+        public static string ParseInput(string prevoiusInput, string currentInput)
+        {
+            //prevoiusInput = "5";
+            //currentInput = "7";
+
+            var finalOutput = String.Concat(prevoiusInput, currentInput);
+            
+            return finalOutput;
+        }
+    }
+}
